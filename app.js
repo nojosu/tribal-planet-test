ffe4cbaaebf841a5591db41d5d59199f535f3d3a
Josué R.H.G <josue.hernandez9459@alumnos.udg.mx>
2021-01-17 17:12:04 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
+
+// Use a static JSON file as our database (for now)
+const file = './db.json'
+
+const channels = require(file)
+console.dir(channels)
+
+// API
+app.get('/api/channels', (request, response) => {
+    response.json(channels)
+})
+
+const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
